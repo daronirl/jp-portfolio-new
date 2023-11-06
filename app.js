b931dce96f25723a260094e895a1f71b268cac9d
daronirl <jprescottirl@gmail.com>
2023-11-06 00:51:53 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector('.hamburger');
+const navBar = document.querySelector('.nav-links');
+
+
+hamburger.addEventListener('click', () => {
+    navBar.classList.toggle('active');
+});
+
+
+
